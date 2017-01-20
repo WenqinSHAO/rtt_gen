0a5568ca62c7a785683744d97d5a78570bfdce41
Wenqin SHAO <samuelshwq@gmail.com>
2017-01-20 15:28:54 +0100

    first finish

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # What is it about?
-Generate artifcial Internet Round-Trip Time time series.
+Generate artificial Internet Round-Trip Time time series.
 
 # How to use it?
+An example usage is provided in [example.R](example.R).
+With the __rtt.gen__ function provided in [rtt_gen.R](rtt_gen.R), one can
+generate random RTT traces of a given length.
+```R
+# in the same folder of rtt_gen.R
+source('rtt_gen.R')
+# generates random RTT trace having 1000 data points
+sample.rtt <- rtt.gen(1000)
+```
+__sample.rtt$trace__ contains the RTT trace, while in __sample.rtt$cpt__
+indexes where a significant change happens to the generated RTT trace are flagged
+to 1.
+
+# How RTT trace is generated?
+1. generate the number of stages;
+2. generate the length for each stage;
+3. generate the RTT baseline value for each stage;
+4. add additional noises/deviation to the RTT baseline;
+5. add relatively long during congestion.
--- rtt_gen.R
+++ rtt_gen.R
@@ -17,7 +17,8 @@ k.stage <- function(n) {
   #   with larger n, the generated traces tend to have more stages
   #   there should as well be a reasonable up-bound for stage numbers
   #   we consider a shortest stage of 10 data points, the max k should be around floor(n/10)
-  
+  r = rpois(1, floor(sqrt(n)/log(n)))
+  return(ifelse(r < floor(n/10), r, floor(n/10)))
 }
 
 len.stage <- function(n, k) {
@@ -34,7 +35,17 @@ len.stage <- function(n, k) {
   #   Possible patterns:
   #   1/ one or two dominant stages (paths); with several short living ones (first implement this case)
   #   2/ mixture of dominant stages (paths); and LB-like equivelant short living paths
-
+  if(k>1) {
+    e <- floor(n/(k*(k-1)))
+    len <- vector()
+    for(i in seq_len(k-1)) {
+      len <- append(len, floor(runif(1, 50, n/k+e)))
+    }
+    len <- append(len, n-sum(len))
+    return(sample(len))
+  } else {
+    return(n)
+  }
 }
 
 baseline.stage <- function(k) {
@@ -48,6 +59,13 @@ baseline.stage <- function(k) {
   #
   # Note:
   #   the RTT difference among stages should be within a reasonable range
+  baseline <- runif(1, 20, 200)
+  if (k > 1){
+    for (i in 2:k){
+      baseline[i] <- baseline[i-1] + ((rgeom(1,0.5)+1) * runif(1, 3, 7) * sample(c(1, -1), 1))
+    }
+  }
+  return(baseline)
 }
 
 stage.trace <- function(len, baseline) {
@@ -59,11 +77,16 @@ stage.trace <- function(len, baseline) {
   #
   # Returns:
   #   vector of float, n in length, containing k stages
+  trace = vector()
+  for (i in seq_len(length(len))) {
+    trace <- append(trace, rep(baseline[i], len[i]))
+  }
+  return(trace)
 }
 
 noise.trace <- function(trace, len) {
   # given a trace with stages, add deviations or spikes due to transient congestion, system load, etc.
-  # 
+  #
   # Args:
   #   trace (vector of float): the stage.trace
   #   len (vector of int): the length of each stage
@@ -78,7 +101,18 @@ noise.trace <- function(trace, len) {
   #   spikes can be large but short living
   #   background noises could be relatively small but lasts for a while
   #   should these noises be considered change, in general no
-  
+  for (i in seq(length(len))) {
+    idx = (sum(len[1:i])-len[i]) + seq(1, len[i], 1)
+    trace[idx] <- trace[idx] + rexp(len[i], rate=runif(1, 0.5, 5))
+  }
+  return(trace)
+}
+
+test.noise.trace <- function(n) {
+  k <- k.stage(n)
+  len <- len.stage(n, k)
+  stage <- stage.trace(len, baseline.stage(k))
+  return(noise.trace(stage, len))
 }
 
 congest.trace <- function(trace, len) {
@@ -89,9 +123,66 @@ congest.trace <- function(trace, len) {
   #   len (vector of int): the length of each stage
   #
   # Returns:
-  #   vector of float, n in length
+  #   data.frame
+  #     column 'trace': RTT ts, vector of float, n in length
+  #     column 'cpt': idx when congestion happens, vecotr of int, 0,1 status
   #
   # Note:
   #   length, location, amplitude of congestion should be generated
-  
+  cpt <- rep(0, length(trace))
+  for (i in seq(length(len))) {
+    # alpha is prob of entering congestion
+    alpha <- min(abs(rnorm(1, mean=0, sd=.005)), .01)
+    # beta is proba of exiting congestion
+    beta <- min(max(abs(rnorm(1, mean=0, sd=.1)), .01), .1)
+    cong.stat <- F
+    cong.amplitude <- 0
+    idx = (sum(len[1:i])-len[i]) + seq(1, len[i], 1)
+    for (j in idx){
+      if (! cong.stat) {
+        if (rbinom(1,1,alpha) == 1) {
+          # enter congestion
+          cong.stat <- T
+          if ((j+1)<length(trace)){
+            cpt[j+1] <- 1
+          }
+          cong.amplitude <- (rgeom(1,0.5)+1) * runif(1, 20, 50) # amplitude of congestion segment
+        }
+      } else {
+        trace[j] <- trace[j] + rpois(1, cong.amplitude)
+        if (rbinom(1,1,beta) == 1) {
+          # leaving congestion
+          cong.stat <- F
+          if ((j+1)<length(trace)){
+            cpt[j+1] <- 1
+          }
+        }
+      }
+    }
+  }
+  return(data.frame(trace=trace, cpt=cpt))
+}
+
+rtt.gen <- function(n) {
+  # generate random RTT time series
+  #
+  # Args:
+  #   n (int): length of the RTT trace
+  #
+  # Returns:
+  #   data.frame
+  #     column 'trace': RTT ts, vector of float, n in length
+  #     column 'cpt': idx when change happens, vecotr of int, 0, 1 status; 1 for change
+  #
+  k <- k.stage(n)
+  len <- len.stage(n, k)
+  stage <- stage.trace(len, baseline.stage(k))
+  noise <- noise.trace(stage, len)
+  congest <- congest.trace(noise, len)
+  if (k > 1) {
+    for (i in seq_len(k-1)) {
+      congest$cpt[sum(len[1:i])+1] <- 1
+    }
+  }
+  return(congest)
 }
